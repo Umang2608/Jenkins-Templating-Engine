2e284199ffc9cdba14a3eb2209895be5188b7040
Sitaramreddyk2020 <64198521+Sitaramreddyk2020@users.noreply.github.com>
2020-04-26 16:57:54 +0530

    Create template.groovy

---
+++ script/template.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+    stages {
+        stage ('Checkout') {
+            steps {
+                git credentialsId: 'sitaram', url: 'https://github.com/Sitaramreddyk2020/Jenkins-Templating-Engine.git'
+                }
+        }
+ 
+        stage ('Maven Build') {
+            steps {
+            sh 'mvn clean package'
+            }
+           
+        }
+         stage('deploy_to dev'){
+         steps {
+         sh 'sshpass -p root scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/template.txt root@35.225.78.93:/tmp'
+                               
+      }
+   }
+         stage('deploy_to qa'){
+         steps {
+         sh 'sshpass -p root scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/template.txt root@34.69.151.189:/tmp'
+                               
+      }
+   }
+        }
+  
+}
