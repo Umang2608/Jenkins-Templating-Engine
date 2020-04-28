8e9c3a723b8c3d4dacc191c793457ae6ffc5624b
Sitaramreddyk2020 <64198521+Sitaramreddyk2020@users.noreply.github.com>
2020-04-28 13:48:54 +0530

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,6 +1,7 @@
 libraries{
     merge = true
     sonarqube
+    ansible
     junit
     jinja2
 }
@@ -10,5 +11,14 @@ stages{
         static_code_analysis
         junit
         jinja2
+    }
+}
+application_environments{
+    dev{
+        ip_address = [ "34.70.70.84" ]
+    }
+    qa{
+        long_name = "Quality"
+        ip_addresses = [ "104.154.81.224" ]
     }
 }
