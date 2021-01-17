db1358f617c2796b2b6e0a519729c9bbf58af424
Umang2608 <67495145+Umang2608@users.noreply.github.com>
2021-01-17 20:39:52 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,21 +1,10 @@
+/*
+  specify which libraries to load: 
+    In the Governance Tier configuration file, 
+    these should be configurations common across 
+    all apps governed by this config. 
+*/
 libraries{
-    merge = true
-    sonarqube
-    junit
-    jinja2
-}
-stages{
-    continuous_integration{
-        build
-        static_code_analysis
-        junit
-        jinja
-    }
-     dev_deployment{
-      steps {
-        retry(x) { // It Retries x number of times mentioned until its successful
-          sh './dev-deploy.sh'
-        }
-      }
-    }
+  merge = true 
+  sonarqube
 }
