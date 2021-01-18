63280d94d787b7d6dc8fc7e1fa84fb5101c25b6d
Umang2608 <67495145+Umang2608@users.noreply.github.com>
2021-01-18 10:31:42 +0530

    Delete pipeline_config.groovy

--- pipeline_config.groovy
+++
@@ -1,9 +0,0 @@
-/*
-  specify which libraries to load: 
-    In the Governance Tier configuration file, 
-    these should be configurations common across 
-    all apps governed by this config. 
-*/
-libraries{
-  maven
-}
